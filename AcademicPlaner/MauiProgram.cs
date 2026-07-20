ad8860e1f9311acb56be5d29935c25430e3e3f1f
Natalia Jewtoszuk <nataliajewtoszuk@gmail.com>
2026-07-21 00:03:13 +0200

    Add sidebar icons and fix responsive layout issues
    - Add FontAwesome icons (FontImageSource) for menu buttons
    - Fix column/row sizing to prevent content clipping on window resize
    - Add ScrollView to right panel for overflow safety
    - Add icon labels with separate button style

--- AcademicPlaner/MauiProgram.cs
+++ AcademicPlaner/MauiProgram.cs
@@ -13,6 +13,11 @@ namespace AcademicPlaner
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+                    fonts.AddFont("Nunito-Regular.ttf", "NunitoRegular");
+                    fonts.AddFont("Nunito-Bold.ttf", "NunitoBold");
+                    fonts.AddFont("Font Awesome 7 Free-Solid-900.otf", "FontAwesomeSolid");
+                    fonts.AddFont("Font Awesome 7 Free-Regular-400.otf", "FontAwesomeRegular");
                 });
 
 #if DEBUG
