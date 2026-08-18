a9865da4a1d42c60328204d74b71285cb66663bf
1Nath1 <nataliajewtoszuk@gmail.com>
2026-08-18 16:31:41 +0200

    Add auto-focus to next field
    - subjects tab: pressing next/done on keyboard moves focus to next  entry, new row added automatically after filling in the last field
    - assessments tab: same auto-focus between fields, without
      auto-adding new row

---
+++ AcademicPlaner/Behaviors/NextEntryBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Maui.Controls;
+
+namespace AcademicPlaner.Behaviors
+{
+    public class NextEntryBehavior: Behavior<Entry>
+    {
+        public static readonly BindableProperty NextElementProperty = BindableProperty.Create(nameof(NextElement), typeof(VisualElement), typeof(NextEntryBehavior));
+
+        public VisualElement NextElement
+        {
+            get => (VisualElement)GetValue(NextElementProperty);
+            set => SetValue(NextElementProperty, value);
+        }
+        
+        protected override void OnAttachedTo(Entry entry)
+        {
+            base.OnAttachedTo(entry);
+            entry.Completed += OnCompleted;
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            base.OnDetachingFrom(entry);
+            entry.Completed -= OnCompleted;
+        }
+
+        private void OnCompleted(object sender, EventArgs e)
+        {
+            NextElement?.Focus();
+        }
+    }
+}
--- AcademicPlaner/Views/SubjectsView.xaml.cs
+++ AcademicPlaner/Views/SubjectsView.xaml.cs
@@ -9,7 +9,6 @@ public partial class SubjectsView : ContentView
     {
 
 
-
         if (BindingContext is not ViewModels.SubjectsViewModel viewModel) return;
 
         viewModel.AddNewSubject();
@@ -45,5 +44,29 @@ public partial class SubjectsView : ContentView
             var entry = sender as Entry;
             entry.Text = e.OldTextValue; // jezeli nie jest cyfra to wracamy do starej wartosci
         }
+    }
+
+
+    private void OnMaxNbCompleted(object sender, EventArgs e)
+    {
+        if (sender is not Entry entry)
+            return;
+
+        if(entry.BindingContext is not Subject subject)
+            return;
+
+        if(BindingContext is not SubjectsViewModel viewModel)
+            return; 
+        
+        int currentIndex = viewModel.Subjects.IndexOf(subject);
+
+        if(currentIndex<0) 
+            return;
+
+        bool isLastRow = currentIndex == viewModel.Subjects.Count - 1;
+
+
+        if(isLastRow)
+        viewModel.AddNewSubject();
     }
 }
